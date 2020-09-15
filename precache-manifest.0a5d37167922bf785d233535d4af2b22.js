fa47e2beee0d6dbd63ea1fae2edd8043c2c4860b
garethscott <59648675+garethscott@users.noreply.github.com>
2020-09-15 10:55:28 +0100

    Updates

---
+++ precache-manifest.0a5d37167922bf785d233535d4af2b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c3f843c78dbe7e55ce1fc6cdc8dde276",
+    "url": "/Pokemon-App/index.html"
+  },
+  {
+    "revision": "88103c288aa8a015bf29",
+    "url": "/Pokemon-App/static/css/main.84e6e117.chunk.css"
+  },
+  {
+    "revision": "97c247265dcd5d8db41d",
+    "url": "/Pokemon-App/static/js/2.1ffd480c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Pokemon-App/static/js/2.1ffd480c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88103c288aa8a015bf29",
+    "url": "/Pokemon-App/static/js/main.d2916b3e.chunk.js"
+  },
+  {
+    "revision": "76b081e189017540cf38",
+    "url": "/Pokemon-App/static/js/runtime-main.45786448.js"
+  }
+]);
